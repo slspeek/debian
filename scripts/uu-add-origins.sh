15edaab5b5d90af2c38349657a18a84178ddadde
Steven L. Speek <slspeek@gmail.com>
2024-02-21 12:25:38 +0100

    unattended-upgrade vscode activation

--- scripts/uu-add-origins.sh
+++ scripts/uu-add-origins.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
-PATCH=$(mktemp)
-cat << 'EOF' > $PATCH
+sudo patch /etc/apt/apt.conf.d/50unattended-upgrades << 'EOF'
 --- /etc/apt/apt.conf.d/50unattended-upgrades	2022-12-31 21:59:00.000000000 +0100
-+++ 50unattended-upgrades	2024-02-20 13:16:11.805202900 +0100
-@@ -40,6 +40,9 @@
++++ 50unattended-upgrades	2024-02-21 12:18:46.727808863 +0100
+@@ -40,6 +40,12 @@
  //      "o=Debian,a=stable-updates";
  //      "o=Debian,a=proposed-updates";
  //      "o=Debian Backports,a=${distro_codename}-backports,l=Debian Backports";
-+
-+	// For updating Google Chrome
++	
++	// Google Chrome
 +	"o=Google LLC,a=stable";
++
++	// Visual Studio Code
++	"o=code stable,a=stable";
  };
  
  // Python regular expressions, matching packages to exclude from upgrading
 EOF
-sudo patch /etc/apt/apt.conf.d/50unattended-upgrades $PATCH
