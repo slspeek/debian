fac016610d7e159fd64807b54218cd2ac11f1d94
Steven L. Speek <slspeek@gmail.com>
2024-02-21 14:47:35 +0100

    golang

---
+++ scripts/install-golang.sh
@@ -0,0 +1,11 @@
+VERSION=1.22.0
+GO_ARCHIVE=go${VERSION}.linux-amd64.tar.gz
+cd /tmp
+wget https://go.dev/dl/${GO_ARCHIVE}
+if [ -d /usr/local/go ]; 
+then 
+    sudo rm -rf /usr/local/go
+fi
+sudo tar xzf ${GO_ARCHIVE} -C /usr/local
+sudo "echo 'export PATH=/usr/local/go/bin:$PATH' >> /etc/profile"
+
