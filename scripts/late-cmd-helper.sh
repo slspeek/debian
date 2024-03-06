f4aa8fac1bfe2eaa27edcbd4975291fae8950f02
Steven L. Speek <slspeek@gmail.com>
2024-03-06 15:36:58 +0100

    bats fix

--- scripts/late-cmd-helper.sh
+++ scripts/late-cmd-helper.sh
@@ -5,3 +5,19 @@ function run_logged() {
     $CMD_FILE > ${LATE_CMD_LOGGING_DIR}/$(basename ${CMD_FILE}).log 2>&1
 }
 
+declare -A results
+
+for LATE_CMD_NAME in "$@";
+do
+    echo $LATE_CMD_NAME
+    run_logged $LATE_CMD_NAME
+    results[$LATE_CMD_NAME]=$?
+done
+
+(for KEY in ${!results[@]}
+do
+    if test result[$KEY] -ne 0;
+        then echo $KEY failt
+    fi
+done)  
+
