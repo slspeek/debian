3d2136384f600ded32ed07e72511b0ff36b68226
slspeek <slspeek@users.noreply.github.com>
2024-03-06 14:38:28 +0000

    deploy: f4aa8fac1bfe2eaa27edcbd4975291fae8950f02

---
+++ late-cmds.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+function auto-set-shortcuts ()
+{
+   /bin/sh -c '/usr/local/bin/auto-set-shortcuts.sh $(id -nu 1000)'
+}
+
+function chrome-remote-desktop ()
+{
+   /usr/local/bin/install-chrome-remote-desktop.sh
+}
+
+function docker ()
+{
+   /bin/sh -c 'adduser $(id -nu 1000) docker'
+}
+
+function earth-pro ()
+{
+   /usr/local/bin/install-earth-pro.sh
+}
+
+function gists ()
+{
+   /usr/local/bin/download-gists.sh
+}
+
+function golang ()
+{
+   /usr/local/bin/install-golang.sh
+}
+
+function google-chrome ()
+{
+   /usr/local/bin/install-google-chrome.sh
+}
+
+function no-gnome-initial ()
+{
+   /bin/sh -c '/usr/local/bin/disable-gnome-initial-setup.sh $(id -nu 1000)'
+}
+
+function prepare-education-box ()
+{
+   /bin/sh -c 'cd /usr/local/bin && wget https://raw.githubusercontent.com/slspeek/linux-beginners-cursus/main/bin/prepare-education-box.sh && chmod +x prepare-education-box.sh'
+}
+
+function sudo-nopasswd ()
+{
+   /bin/sh -c '/usr/local/bin/sudo-nopasswd.sh $(id -nu 1000)'
+}
+
+function tmux-conf ()
+{
+   /bin/sh -c 'sudo -u $(id -nu 1000) /usr/local/bin/create-tmux-conf.sh'
+   /usr/local/bin/create-tmux-conf.sh
+}
+
+function uu-activate ()
+{
+   /bin/sh -c 'echo unattended-upgrades unattended-upgrades/enable_auto_updates boolean true | debconf-set-selections'
+   dpkg-reconfigure -f noninteractive unattended-upgrades
+}
+
+function uu-add-origins ()
+{
+   /usr/local/bin/uu-add-origins.sh
+}
+
+function vscode ()
+{
+   /usr/local/bin/install-vscode.sh
+}
+
