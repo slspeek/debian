9d4fb3e3c7b9cb0f7eeac57b3bbd3586f62f952e
Steven L. Speek <slspeek@gmail.com>
2024-03-12 16:13:02 +0100

    vscode more quiet

--- scripts/install-vscode.sh
+++ scripts/install-vscode.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-apt-get install -q --yes software-properties-common apt-transport-https curl
+apt-get install -qq --yes software-properties-common apt-transport-https curl
 curl -sSL https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
-add-apt-repository --yess "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
-apt-get -q update || true # ignore cdrom entry that is not yet disabled during the late-cmd execution
-apt-get install -q --yess code
+add-apt-repository -y "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+apt-get -qq update || true # ignore cdrom entry that is not yet disabled during the late-cmd execution
+apt-get install -qq --yes code
