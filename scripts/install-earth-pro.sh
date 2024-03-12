1b935cce9c95d048d51980160de9a1156e03bb93
Steven L. Speek <slspeek@gmail.com>
2024-03-12 15:03:06 +0100

    fix install-earth-pro.sh

--- scripts/install-earth-pro.sh
+++ scripts/install-earth-pro.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -e
 
-sudo --no-verbose wget https://dl.google.com/dl/earth/client/current/google-earth-stable_current_amd64.deb -P /tmp
-sudo apt-get install --yes /tmp/google-earth-stable*.deb
+wget --no-verbose https://dl.google.com/dl/earth/client/current/google-earth-stable_current_amd64.deb -P /tmp
+apt-get install --yes /tmp/google-earth-stable*.deb
