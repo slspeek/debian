1903ef6040eef9d69c81e3821f6a6944709ff013
Steven L. Speek <slspeek@gmail.com>
2024-04-09 14:18:11 +0200

    late-cmd-helper defaults for LATE_CMD_SRC and LATE_CMD_LOGGING_DIR

--- scripts/late-cmd-helper.sh
+++ scripts/late-cmd-helper.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 # set -e 
 
+LATE_CMD_SRC=${LATE_CMD_SRC:-/usr/local/bin/late-cmds.sh}
+LATE_CMD_LOGGING_DIR=${LATE_CMD_LOGGING_DIR:-/var/log/installer-preseed/late-cmd}
+
 function run_logged() {
     CMD_FILE=$1
     $CMD_FILE > ${LATE_CMD_LOGGING_DIR}/$(basename ${CMD_FILE}).log 2>&1
