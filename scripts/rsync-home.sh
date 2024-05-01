27674b1df0c24c9cba4b380cb4ccefe0b0d0653e
Steven L. Speek <slspeek@gmail.com>
2024-05-01 15:02:32 +0200

    rsync less fine grained timestamp

--- scripts/rsync-home.sh
+++ scripts/rsync-home.sh
@@ -6,11 +6,13 @@ BACKUP_USER=$2
 BACKUP_HOST=$3
 BACKUP_DIR=$4
 
-TIMESTAMP=$(date +%Y%m%d-%H%M)
+TIMESTAMP=$(date +%Y%m%d)
 DEFAULT_TARGETDIR=/home/${BACKUP_USER}/${BACKUP_NAME}-backup-${TIMESTAMP}/
 TARGETDIR=${4:-$DEFAULT_TARGETDIR}
 
-ssh -l $BACKUP_USER $BACKUP_HOST mkdir -p $TARGETDIR
+if [ -z "$BACKUP_DIR" ]; then
+    ssh -l $BACKUP_USER $BACKUP_HOST mkdir -p $TARGETDIR
+fi
 RSYNC_CMD="rsync -ar --delete -v" 
 $RSYNC_CMD /home /root /etc ${BACKUP_USER}@${BACKUP_HOST}:${TARGETDIR}
 
