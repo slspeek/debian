ba16f0108fa98ba23abbb26bcdf608ad81c4ed55
Steven L. Speek <slspeek@gmail.com>
2024-08-19 11:49:58 +0200

    install-chrome-remote-desktop uses install-deb-from-url

--- scripts/install-chrome-remote-desktop.sh
+++ scripts/install-chrome-remote-desktop.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 set -e
 
-wget --no-verbose https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb -P /tmp
-apt-get install --yes /tmp/chrome-remote-desktop_current_amd64.deb
+install-deb-from-url.sh https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
