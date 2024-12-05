705d985502276086242d4c4ef08f658319c4646d
Steven L. Speek <slspeek@gmail.com>
2024-12-05 14:58:45 +0100

    persistence partition creation

--- bin/write-iso-to-usb-stick.sh
+++ bin/write-iso-to-usb-stick.sh
@@ -26,18 +26,23 @@ echo usb stick: $USB_STICK;
 echo persistence: $PERSISTENCE;
 )|boxes -d ada-box
 
-if [ -n "$PERSISTENCE" ]; then
-  echo Persistence set
-else
-  echo No Persistence
-fi
+(readlink -e /dev/disk/by-id/usb*|grep -w -q ${USB_STICK}) || exit 3
 
-# TODO: tempdir for /mnt
-sudo sh -c "pv $ISO_IMAGE > $USB_STICK; sync"
+#sudo sh -c "pv $ISO_IMAGE > $USB_STICK; sync"
 if [ -n "$PERSISTENCE" ]; then
-   # TODO: parted
-   sudo mkfs.ext4 -L persistence ${USB_STICK}2
-   sudo mount ${USB_STICK}2 /mnt
-   sudo sh -c 'echo "/home" >> /mnt/persistence.conf'
-   sudo umount /mnt
+   MNT_TEMPDIR=$(mktemp -d)
+   # create new partition and accept default three times
+   sudo fdisk $USB_STICK<<EOF
+n
+
+
+
+
+w
+EOF
+   PERSISTENCE_PART=${USB_STICK}3
+   sudo mkfs.ext4 -L persistence ${PERSISTENCE_PART}
+   sudo mount ${PERSISTENCE_PART} $MNT_TEMPDIR
+   sudo sh -c "echo /home >> $MNT_TEMPDIR/persistence.conf"
+   sudo umount $MNT_TEMPDIR
 fi
