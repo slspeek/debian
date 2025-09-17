ec62c6f37eded84f604c9ce98582aed45baff354
Steven L. Speek <slspeek@gmail.com>
2025-09-17 23:28:48 +0200

    install-vscode.sh: extrepo contrib, non-free repos enabled

--- scripts/install-vscode.sh
+++ scripts/install-vscode.sh
@@ -2,6 +2,8 @@
 set -e
 
 apt-get install -qq --yes extrepo
+sed -i -e '/- non-free/ s/^# //' /etc/extrepo/config.yaml
+sed -i -e '/- contrib/ s/^# //' /etc/extrepo/config.yaml
 extrepo enable vscode
 apt-get -qq update || true # ignore cdrom entry that is not yet disabled during the late-cmd execution
 apt-get install -qq --yes code
