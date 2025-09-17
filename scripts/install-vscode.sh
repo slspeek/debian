3af12380df25a90dc35a2f04270982b1dd6c8142
Steven L. Speek <slspeek@gmail.com>
2025-09-17 21:24:12 +0200

    wip: transition to Debian Trixie

--- scripts/install-vscode.sh
+++ scripts/install-vscode.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-apt-get install -qq --yes software-properties-common apt-transport-https curl
-curl -sSL https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
-add-apt-repository -y "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+apt-get install -qq --yes extrepo
+extrepo enable vscode
 apt-get -qq update || true # ignore cdrom entry that is not yet disabled during the late-cmd execution
 apt-get install -qq --yes code
