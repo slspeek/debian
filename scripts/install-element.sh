1503ce19b087f93968417dab79ab8c99f000d722
Steven L. Speek <slspeek@gmail.com>
2025-09-19 13:08:44 +0200

    workaround for cdrom entry in sources.list

--- scripts/install-element.sh
+++ scripts/install-element.sh
@@ -8,7 +8,7 @@ sudo wget -O /usr/share/keyrings/element-io-archive-keyring.gpg \
 echo "deb [signed-by=/usr/share/keyrings/element-io-archive-keyring.gpg] \
 https://packages.element.io/debian/ default main" \
 | sudo tee /etc/apt/sources.list.d/element-io.list
-sudo apt-get update
+sudo apt-get update || true
 sudo apt-get install -y element-desktop
 
 
--- scripts/install-mattermost.sh
+++ scripts/install-mattermost.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
-curl -fsS -o- https://deb.packages.mattermost.com/setup-repo.sh | bash
-
+apt-get install -qq --yes extrepo
+sed -i -e '/- non-free/ s/^# //' /etc/extrepo/config.yaml
+sed -i -e '/- contrib/ s/^# //' /etc/extrepo/config.yaml
+extrepo enable mattermost
+apt-get -qq update || true # ignore cdrom entry that is not yet disabled during the late-cmd execution
 DEBIAN_FRONTEND=noninteractive apt-get install --yes mattermost-desktop
